03a70d1ba018dcb400a6e21ebedcb9b222636734
Ignacio Brandán <105253055+Ibrandan@users.noreply.github.com>
2023-02-23 15:06:02 -0300

    Arreglo conexion db

--- src/config/db.js
+++ src/config/db.js
@@ -3,4 +3,12 @@ import { config } from "dotenv";
 
 config();
 
-export const pool = createPool(process.env.DATABASE_URL);
+export const pool = createPool({
+    database: "familiabrandan",
+    user: "nxwgdcsrvkc1ju7c1kke",
+    host: "us-east.connect.psdb.cloud",
+    password: "pscale_pw_FX8tyS0V6Dm26YjiDPQfXayiiyo6CYf3CZAMiDgKacf",
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  });
