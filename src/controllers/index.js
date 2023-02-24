32b04bf553bfd5b52c45b577e27dfbf692e34707
Ignacio Brandán <105253055+Ibrandan@users.noreply.github.com>
2023-02-23 21:50:51 -0300

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 import { pool } from "../config/db.js";
 
 export const listCategorie = async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   const [result] = await pool.query(
     `SELECT * FROM carta WHERE categoria = "${req.params.cat}"`
   );
