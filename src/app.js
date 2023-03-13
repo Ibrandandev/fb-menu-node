9422312ca3069a93294c52bf501385e9965dbf38
Ignacio Brandán <113082211+Ibrandandev@users.noreply.github.com>
2023-03-13 12:17:47 -0300

    change urls from whitelist

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const app = Express();
 
 const whitelist = [
   "https://fb-menu-front.vercel.app/",
-  "http://localhost:5500/",
+  "http://localhost:5500",
+  "http://127.0.0.1:5500"
 ];
 
 app.use(cors({ origin: whitelist }));
